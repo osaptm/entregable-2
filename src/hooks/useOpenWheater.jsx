02e7554b7d611fd27903e6254860108b33b7e4ec
Mario Joel Olano Ramirez <exmilder@gmail.com>
2022-10-30 17:31:34 -0500

    Mejoras

--- src/hooks/useOpenWheater.jsx
+++ src/hooks/useOpenWheater.jsx
@@ -7,12 +7,12 @@ const useOpenWheater = (Latitude,Longitude) => {
 
   useEffect(() => {
     if(Latitude!==0 && Longitude!==0){
-      // axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=1592a7c8460296fecc3a98911bef2805`)
-      // .then(json => {         
-      //   setObjOpenWheater(json.data);
-      //   console.log(json.data);
-      // })
-      setObjOpenWheater(obj);
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=1592a7c8460296fecc3a98911bef2805`)
+      .then(json => {         
+        setObjOpenWheater(json.data);
+        console.log(json.data);
+      })
+      //setObjOpenWheater(obj);
     }
   }, [Latitude, Longitude]); 
 
